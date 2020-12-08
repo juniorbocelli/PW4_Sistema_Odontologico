c422b23f85e99eb3f7dcfcc486fa3700cb2ee0c5
juniorbocelli <juniorbocelli@gmail.com>
2020-12-08 11:36:46 -0300

    Adicionou novo cron que manda notificação sobre consultas.

--- src/server.js
+++ src/server.js
@@ -6,6 +6,10 @@ cron.schedule('0 0 * * *', function() {
     console.log('Todos os dias às 00:00 o sistema enviará lembretes para quem tem consulta no dia.');
 });
 
+cron.schedule('*/5 * * * *', function() {
+    console.log('De 5 em 5 minutos o sistema verifica consultas que acontecerão a 2 horas e notifica o paciente.');
+});
+
 // Inicializa o servidor
 app.listen(5000, function(){
     console.log("Rodando na porta 5000!");
