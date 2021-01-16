7c94fa274aad07ea0d1880955b51d5104f75763e
juniorbocelli <juniorbocelli@gmail.com>
2021-01-15 23:45:49 -0300

    Primeiras edições nos arquivos bases e configuração do axio.

--- app/src/index.js
+++ app/src/index.js
@@ -4,12 +4,18 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import * as serviceWorker from "./serviceWorker";
+
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </BrowserRouter>,
+  document.getElementById("root")
 );
+
+serviceWorker.unregister();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
